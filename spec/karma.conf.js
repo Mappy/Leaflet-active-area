037aa2a4b569f8b9152368fa45bb71b61bab3e79
flemenach <frederic.lemenach@mappy.com>
2016-02-02 13:58:23 +0100

    update example page with leaflet 0.7.7

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -1,65 +1,65 @@
 // Karma configuration
 module.exports = function (config) {
 
-	var files = [
-		"bower_components/sinon/lib/sinon.js",
-		"bower_components/expect/index.js",
-		"bower_components/leaflet-dist/leaflet-src.js",
-    "src/leaflet.activearea.js",
-		"spec/suites/**/*.js",
+    var files = [
+        "bower_components/sinon/lib/sinon.js",
+        "bower_components/expect/index.js",
+        "bower_components/leaflet-dist/leaflet-src.js",
+        "src/leaflet.activearea.js",
+        "spec/suites/**/*.js",
         {pattern: "bower_components/leaflet-dist/images/*.png", included: false}
-	];
+    ];
 
-	config.set({
-		// base path, that will be used to resolve files and exclude
-		basePath: '../',
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
-		plugins: [
-			'karma-mocha',
-			'karma-coverage',
-			'karma-phantomjs-launcher',
-			'karma-chrome-launcher'
+        plugins: [
+            'karma-mocha',
+            'karma-coverage',
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher'
         ],
 
-		// frameworks to use
-		frameworks: ['mocha'],
+        // frameworks to use
+        frameworks: ['mocha'],
 
-		// list of files / patterns to load in the browser
-		files: files,
-		exclude: [],
+        // list of files / patterns to load in the browser
+        files: files,
+        exclude: [],
 
-		// test results reporter to use
-		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		reporters: ['dots'],
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['dots'],
 
-		// web server port
-		port: 9876,
+        // web server port
+        port: 9876,
 
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_WARN,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_WARN,
 
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
-		browsers: ['PhantomJS'],
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
 
-		// If browser does not capture in given timeout [ms], kill it
-		captureTimeout: 5000,
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 5000,
 
-		// Continuous Integration mode
-		// if true, it capture browsers, run tests and exit
-		singleRun: true
-	});
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
 };
